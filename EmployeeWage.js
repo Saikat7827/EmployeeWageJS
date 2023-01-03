43ce1629a3102d4847f1f7f74b72802c0cb396f2
Saikat7827 <saikatchakraborty957.sc@gmail.com>
2023-01-03 18:59:41 +0530

    Added : Employee Check weather present or absent

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -1,1 +1,11 @@
-console.log("Welcome Employee Wage Program");
+function getAttendance() {
+
+    let checkStatus =  Math.floor(Math.random() * 10) %  2;
+
+    if(checkStatus == 0) {
+        console.log("Employee is Absent");
+    } else {
+        console.log("Employee is Present");
+    }
+}
+    getAttendance();
